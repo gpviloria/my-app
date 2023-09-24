8bced1a2f02af32bca779a8fb2d7aa9067b7a747
gpviloria <gpviloria.student@ua.edu.ph>
2023-09-24 17:31:36 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import "./App.css";
+import "./styles.css";
+
+function App(){
+  return (
+    <div className="calculator-header">
+      <h1>Welcome to my-app</h1>
+    <div className="calculator-grid">
+      <div className="output" >
+        <h1>0</h1>
+      </div>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>+</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>C</button>
+        <button>0</button>
+        <button>=</button>
+        <button>÷</button>
+    </div>
+    </div>
+  );
+}
+export default App;
